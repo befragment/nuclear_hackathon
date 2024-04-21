ac090c67fe0c16ac05a7eae97158c33f3dd18e81
Petr Rybakov <prybakov@miem.hse.ru>
2024-04-21 14:25:20 +0300

    немножко допилено демо с позой (ну очень хотелось)

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from tkinter import image_names
 import streamlit as st
 import time
 import os
-
+import numpy as np
+from PIL import Image
 from ultralytics import YOLO
 from model.calculating_norm import get_last_directory_contents
 model = YOLO('yolov8m-pose.pt')
@@ -42,23 +43,24 @@ def main():
     st.title("Nuclear Team WebPage")   
     st.write("____")
 
-    image_files = st.file_uploader(
+    image_file = st.file_uploader(
         label="Переместите фотографии ниже :sunglasses:",
         type=['jpg', 'png', 'jpeg', 'gif'],
-        accept_multiple_files=True,
+        # accept_multiple_files=True,
     )
-
+    original_image = Image.open(image_file)
+    original_image_np = np.asarray(original_image).astype(np.uint8)
     button_click = st.button("Вычислить") # returns bool
 
     images_to_process = []
 
     with st.spinner("Нейросеть думает..."):
         if button_click:
-            for image in image_files:
-                images_to_process.append(image.name)
-
+            # for image in image_files:
+            #     images_to_process.append(image.name)
             model(
-                source=images_to_process, show=False,
+                # source=images_to_process, show=False,
+                source=original_image_np, show=False,
                 conf=0.3, save=True
             )
 
